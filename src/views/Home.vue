418c5229cc1368fe3ee25c433500d0d46c34b1b8
Admin <admin@Admins-MacBook-Pro.local>
2022-01-11 16:39:52 +0530

    Changed the content of Home file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="This is a Vue.js sample App"/>
   </div>
 </template>
 
